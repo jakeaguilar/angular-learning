6a57f584e71cde02c974e40f61b7e7d1b24b9134
unknown <aguilarjacob23@gmail.com>
2019-04-11 18:29:02 -0700

    added angular-cli-ghpages package

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -9,20 +9,13 @@ export class AssignmentComponent implements OnInit {
   usernameStatus: string = 'No username was created.';
   userName: string = '';
 
-  // Property Binding with [disabled]
   constructor() {
-
-
   }
 
   ngOnInit() {
-
   }
 
-
-  // Event Binding for "Add Server" Buttons and Server Name
-
-
+  //
   onResetUsername() {
     this.usernameStatus = 'Username was created: ' + this.userName;
     this.userName = '';
